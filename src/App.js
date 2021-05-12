6487083e71c66478a9c17c003e01a6351e983f87
lukaszbielsko <lukeabove@gmail.com>
2021-05-12 08:09:51 +0200

    add retool-prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import Retool from 'react-retool'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>embedded retool app</p>
+      <Retool url="https://hejho.retool.com/embedded/public/429a65fc-196b-4a3c-b7f5-923e8b0f9c7a" />
     </div>
   );
 }
